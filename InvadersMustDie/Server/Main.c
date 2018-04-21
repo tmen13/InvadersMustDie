8b25d8fb6d25292a6f3b7d086485f93b23812901
tony mendes <a21190876@alunos.pt>
2018-04-21 17:18:54 +0100

    minor change

--- InvadersMustDie/Server/Main.c
+++ InvadersMustDie/Server/Main.c
@@ -1,121 +1,23 @@
-#include <windows.h>  
-#include <stdlib.h>  
-#include <string.h>  
-#include <tchar.h>  
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#include <tchar.h>
 
-// Global variables  
-
-// The main window class name.  
-static TCHAR szWindowClass[] = _T("win32app");
-
-// The string that appears in the application's title bar.  
-static TCHAR szTitle[] = _T("Invaders Must Die");
-
-HINSTANCE hInst;
-
-// Forward declarations of functions included in this code module:  
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-int CALLBACK WinMain(
-	_In_ HINSTANCE hInstance,
-	_In_ HINSTANCE hPrevInstance,
-	_In_ LPSTR     lpCmdLine,
-	_In_ int       nCmdShow
-)
-{
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-
-	if (!RegisterClassEx(&wcex))
-	{
-		MessageBox(NULL,
-			_T("Call to RegisterClassEx failed!"),
-			_T("Win32 Guided Tour"),
-			NULL);
-
-		return 1;
-	}
-
-	hInst = hInstance; // Store instance handle in our global variable  
-
-					   // The parameters to CreateWindow explained:  
-					   // szWindowClass: the name of the application  
-					   // szTitle: the text that appears in the title bar  
-					   // WS_OVERLAPPEDWINDOW: the type of window to create  
-					   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)  
-					   // 500, 100: initial size (width, length)  
-					   // NULL: the parent of this window  
-					   // NULL: this application does not have a menu bar  
-					   // hInstance: the first parameter from WinMain  
-					   // NULL: not used in this application  
-	HWND hWnd = CreateWindow(
-		szWindowClass,
-		szTitle,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		500, 100,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-	);
-
-	if (!hWnd)
-	{
-		MessageBox(NULL,
-			_T("Call to CreateWindow failed!"),
-			_T("Win32 Guided Tour"),
-			NULL);
-
-		return 1;
-	}
-
-	// The parameters to ShowWindow explained:  
-	// hWnd: the value returned from CreateWindow  
-	// nCmdShow: the fourth parameter from WinMain  
-	ShowWindow(hWnd,
-		nCmdShow);
-	UpdateWindow(hWnd);
-
-	// Main message loop:  
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return (int)msg.wParam;
-}
-
-//  
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)  
-//  
-//  PURPOSE:  Processes messages for the main window.  
-//  
-//  WM_PAINT    - Paint the main window  
-//  WM_DESTROY  - post a quit message and return  
-//  
-//  
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+//
+//
+// WndProc - Window procedure
+//
+//
+LRESULT
+CALLBACK
+WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	HDC hdc;
-	TCHAR greeting[] = _T("Hello, World!");
+	TCHAR greeting[] = _T("Vamos ter 20 nesta merda");
 
-	switch (message)
+	switch (uMsg)
 	{
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
@@ -127,16 +29,91 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			5, 5,
 			greeting, _tcslen(greeting));
 		// End application-specific layout section.  
-
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
 	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-		break;
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 
 	return 0;
+}
+
+//
+//
+// WinMain - Win32 application entry point.
+//
+//
+int
+APIENTRY
+wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPWSTR lpCmdLine,
+	int nShowCmd)
+{
+	// Setup window class attributes.
+	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(wcex));
+
+	wcex.cbSize = sizeof(wcex);	// WNDCLASSEX size in bytes
+	wcex.style = CS_HREDRAW | CS_VREDRAW;		// Window class styles
+	wcex.lpszClassName = TEXT("INVADERSMUSTDIECLASS");	// Window class name
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);	// Window background brush color.
+	wcex.hCursor = LoadCursor(hInstance, IDC_ARROW); // Window cursor
+	wcex.lpfnWndProc = WndProc;		// Window procedure associated to this window class.
+	wcex.hInstance = hInstance;	// The application instance.
+
+								// Register window and ensure registration success.
+	if (!RegisterClassEx(&wcex))
+		return 1;
+
+	// Setup window initialization attributes.
+	CREATESTRUCT cs;
+	ZeroMemory(&cs, sizeof(cs));
+
+	cs.x = 0;	// Window X position
+	cs.y = 0;	// Window Y position
+	cs.cx = 640;	// Window width
+	cs.cy = 480;	// Window height
+	cs.hInstance = hInstance; // Window instance.
+	cs.lpszClass = wcex.lpszClassName;		// Window class name
+	cs.lpszName = TEXT("Invaders Must Die");	// Window title
+	cs.style = WS_OVERLAPPEDWINDOW;		// Window style
+
+										// Create the window.
+	HWND hWnd = CreateWindowEx(
+		cs.dwExStyle,
+		cs.lpszClass,
+		cs.lpszName,
+		cs.style,
+		cs.x,
+		cs.y,
+		cs.cx,
+		cs.cy,
+		cs.hwndParent,
+		cs.hMenu,
+		cs.hInstance,
+		cs.lpCreateParams);
+
+	// Validate window.
+	if (!hWnd)
+		return 1;
+
+	// Display the window.
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+
+	// Main message loop.
+	MSG msg;
+	while (GetMessage(&msg, hWnd, 0, 0) > 0)
+		DispatchMessage(&msg);
+
+	// Unregister window class, freeing the memory that was
+	// previously allocated for this window.
+	UnregisterClass(wcex.lpszClassName, hInstance);
+
+	return (int)msg.wParam;
 }
